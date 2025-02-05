637c35bc55489215aa01e6f2afc56cc542b5cc3f
Simon Vargas <sjvargas01@gmail.com>
2025-02-05 14:34:14 -0500

    added decorator example simple-logging-dec.py

---
+++ simple-logging-dec.py
@@ -0,0 +1,21 @@
+import logging
+import functools
+
+#set up logging
+logging.basicConfig(level=logging.INFO)
+
+def log_calls(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        logging.info(f'Calling {func.__name__} with {args}, {kwargs}')
+        result = func(*args, **kwargs)
+        logging.info(f'{func.__name__} returned {result}')
+        return result
+    return wrapper
+
+@log_calls
+def add(x, y):
+    return x + y
+
+result = add(3, 5)
+print(result) # Output: 8
