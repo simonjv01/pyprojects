0844b2b8fece8ce906672b5e63b7fdf3d95da22c
Simon Vargas <sjvargas01@gmail.com>
2025-03-31 21:44:11 -0400

    list examples

---
+++ list-example.py
@@ -0,0 +1,8 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+print("\n")
+# Example of a list with numbers
+for value in range(1, 5):
+    print(value)
